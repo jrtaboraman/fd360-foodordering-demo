2f0c5d64048bb0bce7743ae18ed94e3a69c5be66
jrtaboraman <75286357+jrtaboraman@users.noreply.github.com>
2023-12-05 06:50:52 +0800

    Delete fd360 directory

--- fd360/app/head.js
+++
@@ -1,9 +0,0 @@
-const Head = () => (
-  <>
-    <title>Future Digital 360</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.ico" />
-  </>
-);
-
-export default Head;
